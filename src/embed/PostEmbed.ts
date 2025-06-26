e8b29b51d24b59e2b5599e62d3f12e322ed265ee
Andrews54757 <andrews54757@gmail.com>
2025-06-26 19:45:42 -0400

    Fix

--- src/embed/PostEmbed.ts
+++ src/embed/PostEmbed.ts
@@ -173,7 +173,7 @@ export class PostEmbed {
         //         }
         //    }
         const images = entryData.images.filter(i => i.path);
-        const paths: string = [];
+        const paths: string[] = [];
         const files: AttachmentBuilder[] = [];
         await Promise.all(images.map(async (image, i) => {
             if (!image.path) {
