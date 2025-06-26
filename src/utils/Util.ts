0fecb33eca767291661ceff5acd6bdecd69e09f4
Andrews54757 <andrews54757@gmail.com>
2025-06-26 15:30:23 -0400

    Wrong mode

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -230,7 +230,7 @@ export async function processImages(images: Image[], download_folder: string, pr
             .resize({
                 width: 386 * 2,
                 height: 258 * 2,
-                fit: 'fill',
+                fit: 'contain',
                // withoutEnlargement: true,
                 background: { r: 0, g: 0, b: 0, alpha: 0 }
             })
