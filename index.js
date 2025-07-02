b5c07baa5f8386da2691a4491569d5a1ce64fa15
Andrews54757 <andrews54757@gmail.com>
2025-07-01 21:03:51 -0400

    Error handling

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Bot = require('./src/Bot.js')
 const secrets = require('./secrets.json')
 
+process.on('unhandledRejection', error => {
+	console.error('Unhandled promise rejection:', error);
+});
+
 const bot = new Bot()
 bot.start(secrets).then(() => {
   console.log('Bot started')
