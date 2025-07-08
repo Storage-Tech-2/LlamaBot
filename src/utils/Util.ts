a0c36feccb20905c76df48809b8157df7d98b0eb
Andrews54757 <andrews54757@gmail.com>
2025-07-07 22:22:43 -0400

    Undo

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -632,11 +632,7 @@ async function iterateAllMessages(channel: TextBasedChannel, iterator: (message:
                 return;
             }
         }
-        if (messages.size < 100) {
-            messages = await channel.messages.fetch({ limit: 100, before: messages.last()?.id });
-        } else {
-            break; // If we fetched 100 messages, we assume there are no more
-        }
+        messages = await channel.messages.fetch({ limit: 100, before: messages.last()?.id });
     }
 }
 
