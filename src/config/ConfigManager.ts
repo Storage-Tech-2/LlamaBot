a5d11be47041ba6877d99a3828e46e0321002be4
Andrews54757 <andrews54757@gmail.com>
2026-01-04 22:40:47 -0500

    Update

--- src/config/ConfigManager.ts
+++ src/config/ConfigManager.ts
@@ -96,6 +96,7 @@ export class ConfigManager {
      * Sets a configuration value ensuring type safety with Config<T>.
      */
     public setConfig<T>(config: Config<T>, value: T): void {
+        console.log(config.id, value, typeof value);
         if (this.configMap.get(config.id) !== value || typeof value === 'object') {
             this.configChanged = true;
         }
