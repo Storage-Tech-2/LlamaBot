211fb480ca35af042d0b49c592e387543211b4b7
Andrews54757 <andrews54757@gmail.com>
2026-01-24 07:59:20 -0500

    Update

--- src/components/buttons/SkipDescriptionButton.ts
+++ src/components/buttons/SkipDescriptionButton.ts
@@ -106,9 +106,9 @@ export class SkipDescriptionButton implements Button {
             await interaction.deferUpdate();
             guildHolder.getBot().getTempDataStore().removeEntry(taskID);
             if (isImage) {
-                await SetImagesMenu.setAndReply(isAll, submission, interaction, attachmentSetTaskData.toSet);
+                await SetImagesMenu.setAndReply(false, submission, interaction, attachmentSetTaskData.toSet);
             } else {
-                await SetAttachmentsMenu.setAttachmentsAndSetResponse(isAll, submission, attachmentSetTaskData.toSet, interaction);
+                await SetAttachmentsMenu.setAttachmentsAndSetResponse(false, submission, attachmentSetTaskData.toSet, interaction);
             }
         }
 
